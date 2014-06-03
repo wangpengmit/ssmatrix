a4add40d423c08b0775a50619e961b596f2fc780
Peng Wang <t-pew@MSRC-3617241.europe.corp.microsoft.com>
2014-06-03 17:30:40 +0100

    Testing SSReflect

--- first.v
+++ first.v
@@ -1,1 +1,8 @@
-Require Import Arith.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Require Import ssreflect.
+Import ssreflect.SsrSyntax.
+
+Inductive list (A : Type) : Type := nil | cons of A & list A.
