5a80618062b7043efe888dc6fa5b2455089b1f0f
Peng Wang <t-pew@MSRC-3617241.europe.corp.microsoft.com>
2014-06-06 10:31:37 +0100

    Renamed first.v to mxdiff.v

--- mxdiff.v
+++ mxdiff.v
@@ -1,8 +1,18 @@
+(* Require SSReflect 1.5 and MathComp 1.5 *)
+
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 Require Import ssreflect.
 Import ssreflect.SsrSyntax.
+Require Import separable.
+
+Require Import ssralg.
+Import GRing.
+Import eqtype.
+
+Zmodule.eqType : zmodType -> eqType
+
 
 Inductive list (A : Type) : Type := nil | cons of A & list A.
