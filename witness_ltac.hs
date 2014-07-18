d8dc75ff3d97502081fcd44a0cebce7eff7e1c8d
Peng Wang <wangp.thu@gmail.com>
2014-07-18 17:38:46 +0100

    Added witness_ltac.hs, a translator from witness trace to Ltac scripts

---
+++ witness_ltac.hs
@@ -0,0 +1,42 @@
+import Text.ParserCombinators.Parsec
+
+traces = many trace
+trace =
+  do
+    id
+    spaces
+    endBy cmd period.
+           
+
+
+
+csvFile = sepBy line eol
+line = sepBy cell (char ',')
+cell = quotedCell <|> many (noneOf ",\n\r")
+
+quotedCell = 
+  do
+    char '"'
+    content <- many quotedChar
+    char '"' <?> "quote at end of cell"
+    return content
+
+quotedChar =
+  noneOf "\""
+  <|> try (string "\"\"" >> return '"')
+  
+eol =   try (string "\n\r")
+        <|> try (string "\r\n")
+        <|> string "\n"
+        <|> string "\r"
+        <?> "end of line"
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
+
+main =
+  do c <- getContents
+     case parse csvFile "(stdin)" c of
+       Left e -> do putStrLn "Error parsing input:"
+                    print e
+       Right r -> mapM_ print r
